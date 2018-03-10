9e84e5ee8a4559b916a546bc2aa58fa8cfc1e95b
Unknown <ronnypdev@gmail.com>
2018-03-09 20:15:34 -0500

    Merge branch 'BAWW-Nav-build' into dev

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,5 @@ console.log('I will become a huidj by December 28 2018');
 console.log('I will become a huidj by December 28 2018');
 
 console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
