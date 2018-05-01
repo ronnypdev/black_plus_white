91b3505d6cbbf8d41dcd3190644bc17a2669ec57
Ronny Perez <ronnypdev@gmail.com>
2018-05-01 14:09:02 -0400

    added files

--- src/js/app.js
+++ src/js/app.js
@@ -31,3 +31,7 @@ console.log('I will become a huidj by December 28 2018');
 console.log('I will become a huidj by December 28 2018');
 
 console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
