4e2c3c0bd032a60da55fccf43a9ada23765fbd4c
Ronny Perez <ronnypdev@gmail.com>
2018-05-03 18:36:02 -0400

    BAWW-work-sample work sample page implemented

--- src/js/app.js
+++ src/js/app.js
@@ -45,3 +45,5 @@ console.log('I will become a huidj by December 28 2018');
 console.log('I will become a huidj by December 28 2018');
 
 console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
