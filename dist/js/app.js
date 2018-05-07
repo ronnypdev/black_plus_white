3ad2e7679076c436f13fb66f7880cdd5a68eb581
Ronny Perez <ronnypdev@gmail.com>
2018-05-07 13:21:34 -0400

    Final changes implemented

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,41 @@
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
+console.log('I will become a huidj by December 28 2018');
+
 console.log('this is working like shit');
 
 console.log('I will become a excellent front end guy fuck');
